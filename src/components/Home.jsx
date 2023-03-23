9f7a7a108d83ddd341ea7a11c0e935ca1ef6deff
glorylaw <100371169+glorylaw@users.noreply.github.com>
2023-03-23 19:49:03 +0100

    added home image

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from "../assets/Hero.svg"
+
+function Home() {
+  return (
+    
+    <div className='pt-[100px] w-screen '>
+        <img classNaame=' bg-[#004DB3]' src ={Hero} alt =""/>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import{MenuIcon,XIcon} from "@heroicons/react/outline"
 import Techtime from "../assets/techtime.svg"
 
 function Navbar() {
-  
+  const[nav, setNav] = useState(false)
+  const handleClick = ()=> setNav(!nav)
   return (
     <div className='w-screen h-[80px] z-10 bg-[#004DB3] fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between item-center w-full h-full'>
     <div className='flex item-center'>
-      <img className=' h-[7vh] pt-[15px]' src={Techtime} alt=''/>
-    <ul className='hidden md:flex text-white item-left'>
+      <img className=' h-[7vh] pt-[15px] ' src={Techtime} alt=''/>
+    <ul className='pl-[200px] hidden md:flex text-white  '>
     <li>Home</li>
     <li>About Us</li>
     <li>Courses</li>
@@ -19,21 +20,20 @@ function Navbar() {
     </ul>
     </div>
     <div className='hidden md:flex pr-4'>
-    <button className='px-8 py-3 mt-3'>Enroll Now</button>
+    <button className='px-4 py-3 mt-2'>Enroll Now</button>
     </div>
-    <div className='md:hidden'>
-    <MenuIcon className='w-5 pt-5 text-white'/>
+    <div className='md:hidden' onClick={handleClick}>
+      {!nav ? <MenuIcon className='w-5 pt-5 text-white'/>:<XIcon className='w-5 pt-5 text-white'/>}
     </div>
       </div>
-
-      <ul className='absolute bg-[#004DB3] w-full px-8 text-white'>
+      <ul className={!nav ? 'hidden':'absolute bg-[#004DB3] w-full px-8 text-white'}>
       <li className='border-b-2 border-white w-full'>Home</li>
       <li className='border-b-2 border-white w-full'>About Us</li>
       <li className='border-b-2 border-white w-full'>Courses</li>
       <li className='border-b-2 border-white w-full'>Testimonials</li>
       <li className='border-b-2 border-white w-full'>Community</li>
       <div>
-        <button className='px-8 py-3 mt-2 mb-2'>Enroll Now</button>
+        <button className='px-8 py-3 mt-2 mb-4'>Enroll Now</button>
       </div>
       </ul>
         
